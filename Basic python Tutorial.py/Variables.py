80ef33d71b0e299c4709aafb230832b4a1fe3386
Sangita <sangitabilawar5@gmail.com>
2026-01-05 09:47:41 -0800

    first commit

---
+++ Basic python Tutorial.py/Variables.py
@@ -0,0 +1,13 @@
+name = "Sangita Bilawar"
+age = 20
+price = 12.5
+x = 24
+print("name")#strings are always written inside "" and inside "" the string printed as it is.
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(price)
+print(type(price))
+print(x)
+print(type(x))
