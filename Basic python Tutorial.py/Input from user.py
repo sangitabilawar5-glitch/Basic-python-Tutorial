83666a5bd010daae182836648d417cf450106fae
Sangita <sangitabilawar5@gmail.com>
2026-01-06 05:03:48 -0800

    first commit

---
+++ Basic python Tutorial.py/Input from user.py
@@ -0,0 +1,9 @@
+# To get data from user use keyword Input.
+name = input("Enter your name:")
+Number = int(input("Enter number:"))
+x = float(input("Enter value:"))
+isFollow = (input("Enter value:"))
+print("name=",name)
+print("Number=",Number)
+print("x=",x)
+print("isFollow=",isFollow)
---
+++ Basic python Tutorial.py/Type casting.py
@@ -0,0 +1,18 @@
+# Type casting 
+a = "2"
+b = int("2")  #int data type which can be converted into float data type by type casting:
+c = float(6.2)
+sum = b+c
+print(sum)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(sum))
+
+
+a = 3.14
+b = str(a)
+print(type(a))
+print(type(b))
+
+
