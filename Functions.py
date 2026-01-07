1c42656fedf07eb6e4fd0ad98b34a0394d73f616
Sangita <sangitabilawar5@gmail.com>
2026-01-07 02:55:12 -0800

    first commit

---
+++ Functions.py
@@ -0,0 +1,10 @@
+str = "trust on God."
+print(str.endswith("on")) #function endwith is used.
+str = "trust on God."
+str = str.capitalize()    #Function  capitalize is used.
+print(str.capitalize())
+str = "trust on God."
+print(str.replace("on","to")) # on and to are the two different strings.
+print(str.find("t"))          #This function search at what index " t "comes first.
+print(str.count("o"))          #This function count the number of "o" in the whole string.
+
